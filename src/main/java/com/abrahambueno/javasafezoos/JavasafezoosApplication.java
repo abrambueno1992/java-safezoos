1dbc4f919f9916a0baa8fb69f2a496a12516ac00
Abraham Bueno <abueno@localhost.localdomain>
2019-02-27 21:33:55 -0600

    Initial setup

---
+++ src/main/java/com/abrahambueno/javasafezoos/JavasafezoosApplication.java
@@ -0,0 +1,13 @@
+package com.abrahambueno.javasafezoos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavasafezoosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavasafezoosApplication.class, args);
+    }
+
+}
